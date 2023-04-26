fbbc1a341f9ca2c293438bbac63b1b34032cf5e3
Marcel Jar <marcel.jar@senecacollege.ca>
2023-04-26 10:28:55 -0400

    Update helloc.cpp

--- introduction/helloc.cpp
+++ introduction/helloc.cpp
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    char name[20];
+    int age;
+} Person;
+
+void introduce(Person *p) {
+    printf("Hi, my name is %s and I am %d years old.\n", p->name, p->age);
+}
+
 int main() {
-
-   printf("Hello, World!");
-
-   return 0;
+    Person p;
+    strcpy(p.name, "John");
+    p.age = 30;
+    introduce(&p);
+    return 0;
 }
