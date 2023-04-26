6c9811500b35bc00758f59e81c44e65c9efa7f1c
Marcel Jar <marcel.jar@senecacollege.ca>
2023-04-26 10:37:57 -0400

    Update hello.cpp

--- introduction/hello.cpp
+++ introduction/hello.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <string>
+
+class Person {
+private:
+    std::string name;
+    int age;
+public:
+    Person(std::string name, int age) {
+        this->name = name;
+        this->age = age;
+    }
+    void introduce() {
+        std::cout << "Hi, my name is " << name
+            << " and I am " << age << " years old." << std::endl;
+    }
+};
 
 int main() {
-
-    std::cout << "Hello World!" << std::endl;
-    
+    Person p("John", 30);
+    p.introduce();
     return 0;
 }
