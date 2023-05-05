10adf2b58cd3b1889af08d7fe8fa66d32169f94e
Marcel Jar <marcel.jar@senecacollege.ca>
2023-05-04 20:53:26 -0400

    Update class1.cpp

--- classes/class1.cpp
+++ classes/class1.cpp
@@ -7,35 +7,28 @@ class Student{
         char last_name_[16];
         float gpa_;
     public:
-        void edit();
-        void display();
+        void edit() {
+            std::cout << "First name: " <<std::endl;
+            std::cin >> first_name_;
+            std::cout << "Last name: " <<std::endl;
+            std::cin >> last_name_;
+            std::cout << "gpa: " <<std::endl;
+            std::cin >> gpa_;
+        }
+        void display(){
+            std::cout << "-----------------" << std::endl;
+            std::cout << "Name: " << 
+              first_name_ << " " << last_name_ << std::endl;
+            std::cout << "gpa: " << gpa_ << std::endl;
+        }
 };
-
-void Student::edit(){
-    std::cout << "Enter the student's first name: " <<std::endl;
-    std::cin >> first_name_;
-    std::cout << "Enter the student's last name: " <<std::endl;
-    std::cin >> last_name_;
-    std::cout << "Enter the student's gpa: " <<std::endl;
-    std::cin >> gpa_;
-}
-
-void Student::display(){
-    std::cout << "-------------------------" << std::endl;
-    std::cout << "Student's name: " << 
-        first_name_ << " " << last_name_ << std::endl;
-    std::cout << "Student's gpa: " << gpa_ << std::endl;
-}
 
 int main() {
     
-    Student john_doe, jane_smith;
+    Student john_doe;
 
     john_doe.edit();
-    jane_smith.edit();
-
-    john_doe.display();
-    jane_smith.display();
+    john_doe.display();    
     
     return 0;
 }
