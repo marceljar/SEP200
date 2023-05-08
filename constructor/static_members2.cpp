0a99d258a67914da556db7b00ce593111704f12d
Marcel Jar <marcel.jar@senecacollege.ca>
2023-05-08 10:57:54 -0400

    Add files via upload

---
+++ constructor/static_members2.cpp
@@ -0,0 +1,70 @@
+//application that showcases static member functions
+#include <iostream>
+#include <string.h> //defines strcpy
+// uncomment to allow strcpy to work on Visual Studio
+//#pragma warning(disable:4996)
+
+class Student{
+    private:
+        char first_name_[16];
+        char last_name_[16];
+        float gpa_;
+        static int num_students_;
+    public:        
+        Student();
+        ~Student();
+        void edit();
+        void display(); 
+        static void display_num();      
+};
+
+Student::Student(){
+    num_students_++;
+    this->edit();
+}
+
+Student::~Student(){
+    char first_name[16] = {""};
+    char last_name[16] = {""};
+    strcpy(first_name_, first_name);
+    strcpy(last_name_, last_name);
+    gpa_ = 0;
+    num_students_--;
+}
+
+void Student::edit(){
+    std::cout << "First name: " <<std::endl;
+    std::cin >> first_name_;
+    std::cout << "Last name: " <<std::endl;
+    std::cin >> last_name_;
+    std::cout << "gpa: " <<std::endl;
+    std::cin >> gpa_;
+}
+
+void Student::display(){
+    std::cout << "-----------------" << std::endl;
+    std::cout << "Name: " << 
+        first_name_ << " " << last_name_ << std::endl;
+    std::cout << "gpa: " << gpa_ << std::endl;
+}
+
+int Student::num_students_ = 0;
+
+void Student::display_num() {
+    std::cout << "There are " << num_students_ 
+       << " students in the system." << std::endl;
+}
+
+int main() {
+    
+    Student::display_num();
+    
+    Student student1, student2;
+    
+    student1.display(); 
+    student1.display(); 
+
+    Student::display_num();
+    
+    return 0;
+}
